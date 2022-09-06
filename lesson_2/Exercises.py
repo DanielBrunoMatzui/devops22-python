1a38d6d2ab44bd93084874ddfb2c10f66c022010
Daniel Bruno Matzui <Daniel.BrunoMatzui@yh.nackademin.se>
2022-09-06 15:42:46 +0200

    Stage 2

---
+++ lesson_2/Exercises.py
@@ -0,0 +1,35 @@
+# print values
+
+# Print a integer value
+my_int = 4
+print(my_int)
+# Print a float value
+my_float = 4.6
+print(my_float)
+# Print a String
+my_string = "Hello World"
+print(my_string)
+# Print a boolean value
+my_bool = not True
+print(my_bool)
+# Print a None value
+x = None
+print(x)
+# Print a f-string containing all previous values in one line i.e 1, 1.0, "hello" ..
+print(f"{my_int}, {my_float}, {my_string}, {my_bool}, {x}")
+
+#Create a program that uses `input` and type conversion
+#Input a string and assign it to a variable, then print the variable
+my_new_string = input("What's your name?: ")
+print(my_new_string)
+# Input a number and assign it to a variable, print the value doubled
+value = int(input("Double the value: "))
+print(value * 2)
+ 
+
+
+
+
+
+
+

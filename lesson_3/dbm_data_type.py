4ab8fd329da039dc13fabf9d0ea38041a0304ebd
Daniel Bruno Matzui <Daniel.BrunoMatzui@yh.nackademin.se>
2022-09-12 22:39:46 +0200

    new changes

---
+++ lesson_3/dbm_data_type.py
@@ -0,0 +1,94 @@
+# # Exercise 3 Bool - and, or, not
+# # x = True
+# # y = False
+# # z = True
+
+# # 1.
+# # print(x or y or z)
+# # 2.
+# # print(x and y and z)
+# # 3.
+# # print(x and y and z)
+# # 4.
+# # print(x or y or z)
+# # 5.
+# # print(not(z or y))
+# # 6.
+# # print(not(z and y))
+
+# # Exercise 4 Data Structure - List, common sequence operations
+# # 1.
+# my_list = []
+# # 2. 
+# color = "red"
+# # 3.
+# my_list = [color]
+# # print(my_list)
+# # 4.
+# # print(my_list [0])
+# # 5.
+# my_list.append("blue") #The append() method adds one item to the end of the list.
+# my_list.append("brown")
+# # print(my_list)
+# # 6. `x in s`
+# # print(my_list[1]) 
+# # 7. `x not in s`
+# # print(my_list[4])
+# # # 8. `s + t`
+# sec_list = ["yellow", "purple"]
+# my_list = my_list + sec_list 
+# #print (my_list)
+# # 9.`s * n`
+# my_list = ["red ", "red ", "yellow ", "yellow ", "yellow "]
+# #print(my_list[0] * 2)
+# # print(my_list[2] * 3)
+# # 10. `s[i:j]`
+# #print(my_list[0:4])
+# # 11. `s.count(x)`
+# #print(my_list.count("red "))
+# # 12. operation `s.index("yellow")`
+# elementIndex = my_list.index("yellow ") # The element parameter is required and is the item searched for in the list. It searches an element in the list and returns its index.
+# #print(elementIndex)
+# # 13. operation `len(s)`
+# #print, len(my_list)
+# # 14.
+# my_list = [8, 5, 7, 4, 9, 1, 3]
+# #     1. Print the min value in the list
+# my_list = min(my_list)
+# #print(my_list)
+# #     2. Print the max value in the list
+# my_list = max(my_list)
+# #print(my_list)
+
+# Exercise 5 Sorting
+#1. + 3.
+#cars = ["volvo", "audi", "volkswagen", "porsche", "bmw", "ferrari", "mercedes", "cooper", "fiat", "lexus"]
+#print(sorted(cars))
+#print(sorted(cars, reverse= True))
+#2. + 3.
+# cars.sort()
+# print(cars)
+# cars.sort(reverse= True)
+# print(cars)
+#4.
+cars = [("volvo", "xc60"), ("audi", "e-tron"), ("volkswagen", "t-roc"), ("porsche", "taycan"), ("bmw", "x5")]
+cars.sort()
+print(cars[::2])
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
---
+++ playing_around.py
@@ -0,0 +1,53 @@
+#If Else statements two diffrent ways
+# a = 9
+# b = 8
+# if a < b:
+#     print("a is less then b")
+# elif a == b: # == equal to meanwhile = assigns the right handside to the left handside
+#     print ("a is equal to b")
+# else:
+#     print("a is greater then b")
+
+# if a < b:
+#     print("a is less then b")
+# else:
+#     if a == b:
+#         print("a is equal to b")
+#     else:
+#         print("a is greater then b")
+
+
+# Create a bmi calculater
+
+# name = "Bruno"
+# weight_kg = 66
+# height_cm = 173
+
+# bmi = weight_kg / height_cm / height_cm * 10000
+# print("bmi :")
+# print(bmi)
+
+# if bmi < 18.5:
+#      print(name)
+#      print("You are underweight")
+# elif bmi < 24.9:
+#      print(name)
+#      print("you are normalweight")
+# elif bmi < 29.9:
+#      print(name)
+#      print("you are overweight")
+# else:
+#      print(name)
+#      print("you are fat")
+
+# Swapping two variabels adding one variabel
+
+# t1 = "Hello"
+# t2 = "World"
+
+# temp = t1
+# t1 = t2
+# t2 = temp
+
+# print (t1)
+# print(t2)

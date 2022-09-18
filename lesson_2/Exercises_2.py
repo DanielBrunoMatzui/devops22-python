167def640a6f025a37eb0e4c1467d79f98538fb6
Daniel Bruno Matzui <Daniel.BrunoMatzui@yh.nackademin.se>
2022-09-18 22:44:59 +0200

    New changes

--- lesson_2/Exercises_2.py
+++ lesson_2/Exercises_2.py
@@ -1,39 +1,34 @@
-# print values
-
-# Print a integer value
-my_int = 4
+#Exercise printing
+#1.
+my_int = 5
 print(my_int)
-# Print a float value
-my_float = 4.6
-print(my_float)
-# Print a String
-my_string = "Hello World"
+#2.
+my_float =  5.5
+print (my_float)
+#3.
+my_string = "hello world"
 print(my_string)
-# Print a boolean value
-my_bool = not True
+#4.
+my_bool = True
 print(my_bool)
-# Print a None value
-x = None
-print(x)
-# Print a f-string containing all previous values in one line i.e 1, 1.0, "hello" ..
-print(f"{my_int}, {my_float}, {my_string}, {my_bool}, {x}")
+#5.
+my_none = None
+print(my_none)
+#6.
+print(f"{my_int}, {my_float}, {my_string}, {my_bool}, {my_none}")
 
-#Create a program that uses `input` and type conversion
-#Input a string and assign it to a variable, then print the variable
-my_new_string = input("What's your name?: ")
-print(my_new_string)
-# Input a number and assign it to a variable, print the value doubled
-value = int(input("Double the value: "))
-print(value * 2)
-#Input a string i.e `hello` and assign it to a variable, print the string repeated `hellohello`
-new_string = input("Greetings: ")
-print(new_string * 2)
-#Input a float and assign it to a variable, print the value divided by 3.5
-new_value = float(input("Divided the value by 3.5: "))
-print(new_value / 3.5)
-
-
-
-
+#Exercise input
+#1.
+my_string = input("What's your name?: ")
+print(my_string)
+#2. 
+my_int = int(input("Double the value: "))
+print(my_int*2)
+3.
+my_string = input("Greetings: ")
+print(my_string*2)
+#4.
+my_float = float(input("Divide the value: "))
+print(my_float / 3.5)
 
 

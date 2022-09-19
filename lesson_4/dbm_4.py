09834e2453cf7ae3a713a34ce6ea068576094612
Daniel Bruno Matzui <Daniel.BrunoMatzui@yh.nackademin.se>
2022-09-19 16:42:56 +0200

    new changes

---
+++ lesson_4/dbm_4.py
@@ -0,0 +1,52 @@
+#Exercise 1 even or odd
+#1. 2. 3. 
+# num = int(input("Write a random number: "))
+# if num % 22 == 0:
+#     print("even")
+# else:
+#     print("odd")
+
+#Exercise 2 vip
+#1. 
+# my_name = input("What's your name: ")
+# names = ["dan", "ben", "jen", "adam", "emma"]
+# if my_name in names:
+#     print(f"Welcome, {my_name}, your on the list")
+# else:
+#     print("Sorry, you are not on the list")
+
+#Exercise 3 repeat word
+#1. 
+# word = input("Write a word: ")
+# for x in range(len(word)):
+#     print(word)
+
+#Exercise 4 Sum 
+#1. 2. 3. 
+# start = int(input("Start number: "))
+# stop = int(input("Stop number: "))
+
+# total = 0
+# for num in range(start, stop):
+#     print(num)
+#     total += num
+# print(f"sum:{total}")
+
+#Exercise 5
+#1. 2. 3. 4. 5.
+
+while True:
+    text = input("Write a word or write stop to quit: ")
+    if text == "stop":
+        break
+    print(f"{text} has length {len(text)}")
+
+
+
+
+
+
+
+
+
+
